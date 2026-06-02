92caabb7d9050bcccbe5d6d41b0e1e9f5c664820
mboya <mboyaberry@gmail.com>
2026-06-02 16:26:44 +0300

    Enable JWT authentication on Tribe model.
    Uses denylist revocation and removes rememberable in favor of bearer tokens.

--- app/models/tribe.rb
+++ app/models/tribe.rb
@@ -6,8 +6,9 @@ class Tribe < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable, :trackable
+         :recoverable, :validatable,
+         :confirmable, :lockable, :trackable,
+         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
   has_paper_trail skip: %i[
     encrypted_password
