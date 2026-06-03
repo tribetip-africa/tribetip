12370ea73277e7bf3e7f9e8bf77d2df81f458c61
mboya <mboyaberry@gmail.com>
2026-06-03 13:23:07 +0300

    Register Rack::Attack middleware for the API.

--- config/application.rb
+++ config/application.rb
@@ -40,6 +40,7 @@ module Tribetip
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
     config.middleware.use Rack::Attack
 
     # Ensure new models/migrations use UUID keys by default.
