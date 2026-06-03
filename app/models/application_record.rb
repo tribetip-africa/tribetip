3e767d199b37299a98548376f066f605b121d3c2
mboya <mboyaberry@gmail.com>
2026-06-03 13:23:07 +0300

    Connect models to primary and read replica databases.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  unless Rails.env.test?
+    connects_to database: { writing: :primary, reading: :primary_replica }
+  end
 end
