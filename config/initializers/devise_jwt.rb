a67e803877336072841e9462b42b7e2075f687f9
mboya <mboyaberry@gmail.com>
2026-06-08 21:25:32 +0300

    Update config/initializers/devise_jwt.rb.
    Modify this configuration (1 edited section, 18 additions, 0 removals).
    
    What changed:
    - Adjust implementation and supporting logic in this configuration.

---
+++ config/initializers/devise_jwt.rb
@@ -0,0 +1,18 @@
+Devise.setup do |config|
+  config.jwt do |jwt|
+    jwt.secret = if Rails.env.production?
+      ENV.fetch("DEVISE_JWT_SECRET_KEY")
+    else
+      ENV.fetch("DEVISE_JWT_SECRET_KEY") do
+        Rails.application.credentials.devise_jwt_secret_key || Rails.application.secret_key_base
+      end
+    end
+    jwt.dispatch_requests = [
+      [ "POST", %r{^/tribes/sign_in} ]
+    ]
+    jwt.revocation_requests = [
+      [ "DELETE", %r{^/tribes/sign_out} ]
+    ]
+    jwt.expiration_time = 1.day.to_i
+  end
+end
