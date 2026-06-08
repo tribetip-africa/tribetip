2cbed3bc018e65f54a6f4e55c23b26d49c1a272b
mboya <mboyaberry@gmail.com>
2026-06-08 21:25:32 +0300

    Update app/controllers/tribes/sessions_controller.rb.
    Modify this controller (1 edited section, 87 additions, 0 removals).
    
    What changed:
    - Introduce Tribes.
    - Introduce SessionsController.
    - Add reject_suspended_sign_in method.
    - Add normalize_sign_in_params method.
    - Add sign_in_tribe_params method.
    - Add raw_sign_in_json_params method.
    - Add respond_with(resource, method.
    - Add respond_to_on_destroy(_opts method.
    - Add tribe_payload(tribe) method.
    - Include DatabaseRouting.
    - Include SecureHttpCaching.
    - Include Tribetip::Errors::Handler.

---
+++ app/controllers/tribes/sessions_controller.rb
@@ -0,0 +1,87 @@
+module Tribes
+  class SessionsController < Devise::SessionsController
+    include DatabaseRouting
+    include SecureHttpCaching
+    include Tribetip::Errors::Handler
+    include TribeSerializable
+    include AuthenticatedTribe
+
+    respond_to :json
+
+    prepend_before_action :normalize_sign_in_params, only: :create
+    prepend_before_action :reject_suspended_sign_in, only: :create
+
+    private
+
+    def reject_suspended_sign_in
+      login = params.dig(:tribe, :login)&.to_s&.strip&.downcase
+      password = params.dig(:tribe, :password)
+      return if login.blank? || password.blank?
+
+      tribe = Tribe.find_for_database_authentication(login: login)
+      return unless tribe&.valid_password?(password)
+      return unless tribe.suspended?
+
+      render_error(Tribetip::Errors::Authorization.new("Account suspended."))
+    end
+
+    def normalize_sign_in_params
+      tribe = sign_in_tribe_params
+      return if tribe.blank?
+
+      login = tribe["login"].presence || tribe[:login].presence
+      password = tribe["password"].presence || tribe[:password]
+      return if login.blank? || password.blank?
+
+      params[:tribe] = ActionController::Parameters.new(
+        login: login.to_s.strip.downcase,
+        password: password
+      ).permit(:login, :password)
+    end
+
+    # :email and :password match filter_parameters and become "[FILTERED]" in params.
+    def sign_in_tribe_params
+      return raw_sign_in_json_params if request.content_type.to_s.include?("application/json")
+
+      params.to_unsafe_h["tribe"] || params.to_unsafe_h[:tribe] || {}
+    end
+
+    def raw_sign_in_json_params
+      rack_input = request.env["rack.input"]
+      return {} unless rack_input
+
+      rack_input.rewind if rack_input.respond_to?(:rewind)
+      body = rack_input.read
+      rack_input.rewind if rack_input.respond_to?(:rewind)
+      return {} if body.blank?
+
+      JSON.parse(body).fetch("tribe", {})
+    rescue JSON::ParserError
+      {}
+    end
+
+    def respond_with(resource, _opts = {})
+      apply_http_cache_policy(:no_store)
+      render json: tribe_payload(resource), status: :ok
+    end
+
+    def respond_to_on_destroy(_opts = {})
+      apply_http_cache_policy(:no_store)
+      if current_tribe
+        render json: { message: "Signed out successfully." }, status: :ok
+      else
+        render_error(Tribetip::Errors::Authentication.new("No active session."))
+      end
+    end
+
+    def tribe_payload(tribe)
+      token, _payload = Warden::JWTAuth::UserEncoder.new.call(tribe, :tribe, nil)
+
+      {
+        message: "Signed in successfully.",
+        token: token,
+        tribe: tribe_json(tribe)
+      }
+    end
+  end
+end
