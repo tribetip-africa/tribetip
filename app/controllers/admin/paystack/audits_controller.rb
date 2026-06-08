ef0a8fbeb104811c4991be0d71e0a82c618e2b77
mboya <mboyaberry@gmail.com>
2026-06-08 21:25:31 +0300

    Update app/controllers/admin/paystack/audits_controller.rb.
    Modify this controller (1 edited section, 39 additions, 0 removals).
    
    What changed:
    - Introduce Admin.
    - Introduce Paystack.
    - Introduce AuditsController.
    - Add show method.
    - Add set_tribe method.
    - Add sync_requested? method.
    - Add before_action set_tribe.
    - Log privileged admin actions.

---
+++ app/controllers/admin/paystack/audits_controller.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module Admin
+  module Paystack
+    class AuditsController < BaseController
+      before_action :set_tribe
+
+      def show
+        authorize @tribe, :audit_paystack?
+
+        apply_http_cache_policy(:no_store)
+        report = Tribetip::Paystack::AuditOnboarding.call(@tribe, sync: sync_requested?)
+
+        if sync_requested?
+          record_admin_audit!(
+            action: "paystack_audit_sync",
+            target: @tribe,
+            details: {
+              healthy: report.healthy,
+              onboarding_complete: report.onboarding_complete
+            }
+          )
+        end
+
+        render json: { audit: report.as_json }
+      end
+
+      private
+
+      def set_tribe
+        @tribe = Tribe.find(params[:id])
+      end
+
+      def sync_requested?
+        ActiveModel::Type::Boolean.new.cast(params[:sync])
+      end
+    end
+  end
+end
