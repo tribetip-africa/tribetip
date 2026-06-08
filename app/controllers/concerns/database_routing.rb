5e16b55ab7d6ee20b1929983295a9a2fcf43a456
mboya <mboyaberry@gmail.com>
2026-06-08 21:25:31 +0300

    Update app/controllers/concerns/database_routing.rb.
    Modify this controller (1 edited section, 47 additions, 0 removals).
    
    What changed:
    - Introduce DatabaseRouting.
    - Add route_database_by_request method.
    - Add force_primary_connection? method.
    - Add authenticated_api_request? method.
    - Add devise_auth_request? method.

---
+++ app/controllers/concerns/database_routing.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module DatabaseRouting
+  extend ActiveSupport::Concern
+
+  included do
+    around_action :route_database_by_request
+  end
+
+  private
+
+  def route_database_by_request
+    if force_primary_connection?
+      ActiveRecord::Base.connected_to(role: :writing) { yield }
+    else
+      yield
+    end
+  end
+
+  def force_primary_connection?
+    return true if request.path == "/up"
+    return true if devise_auth_request?
+    return true if authenticated_api_request?
+
+    request.post? || request.put? || request.patch? || request.delete?
+  end
+
+  def authenticated_api_request?
+    return true if request.path.start_with?("/me/")
+
+    authorization = request.headers["Authorization"]
+    authorization.present? && authorization.start_with?("Bearer ")
+  end
+
+  def devise_auth_request?
+    path = request.path
+    return true if path.start_with?(
+      "/tribes/sign_in",
+      "/tribes/sign_out",
+      "/tribes/password",
+      "/tribes/confirmation",
+      "/tribes/unlock"
+    )
+
+    request.post? && path == "/tribes"
+  end
+end
