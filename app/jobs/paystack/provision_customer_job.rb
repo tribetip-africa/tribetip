3a1bc2b3e8673710ef7595a75271507129e142cf
mboya <mboyaberry@gmail.com>
2026-06-08 21:25:32 +0300

    Update app/jobs/paystack/provision_customer_job.rb.
    Modify this background job (1 edited section, 21 additions, 0 removals).
    
    What changed:
    - Introduce Paystack.
    - Introduce ProvisionCustomerJob.
    - Add perform(tribe_id) method.

---
+++ app/jobs/paystack/provision_customer_job.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Paystack
+  class ProvisionCustomerJob < ApplicationJob
+    queue_as :paystack
+
+    limits_concurrency to: 1, key: ->(tribe_id) { "paystack-customer/#{tribe_id}" }, duration: 10.minutes
+
+    def perform(tribe_id)
+      tribe = Tribe.find_by(id: tribe_id)
+      return unless tribe
+
+      run_job_step(tribe_id: tribe_id) do
+        result = Tribetip::Paystack::ProvisionCustomer.call(tribe)
+        raise StandardError, result.message unless result.success?
+
+        result
+      end
+    end
+  end
+end
