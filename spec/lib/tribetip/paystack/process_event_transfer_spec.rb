2b2a40b372802758cd261dca41ab89472bdafb47
mboya <mboyaberry@gmail.com>
2026-06-11 11:58:29 +0300

    Add specs for transfer event handling in ProcessEvent.
    - Verify transfer webhooks delegate to RecordSettlement.

---
+++ spec/lib/tribetip/paystack/process_event_transfer_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Tribetip::Paystack::ProcessEvent do
+  def create_tribe(username: "transfer_event")
+    tribe = Tribe.create!(
+      email: "#{username}@tribetip.africa",
+      password: "securepass123",
+      password_confirmation: "securepass123",
+      username: username,
+      country_code: "KE",
+      currency: "KES"
+    )
+    complete_stub_paystack_onboarding!(tribe)
+    tribe.reload
+  end
+
+  it "records settlements for transfer.success webhooks" do
+    tribe = create_tribe
+    event = {
+      "event" => "transfer.success",
+      "data" => {
+        "transfer_code" => "TRF_process_event",
+        "amount" => 80_000,
+        "currency" => "KES",
+        "status" => "success",
+        "metadata" => {
+          "tribe_id" => tribe.id
+        },
+        "recipient" => {
+          "details" => {
+            "account_number" => "0712345678",
+            "bank_name" => "M-PESA"
+          }
+        }
+      }
+    }
+
+    described_class.call(event)
+
+    settlement = PaystackSettlement.find_by(paystack_transfer_code: "TRF_process_event")
+    expect(settlement).to be_present
+    expect(settlement.tribe_id).to eq(tribe.id)
+    expect(settlement.status).to eq("success")
+  end
+end
