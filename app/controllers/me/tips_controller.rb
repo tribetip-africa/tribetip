37567c9b64d16ef1b08741b86f5c41e40c3d3881
mboya <mboyaberry@gmail.com>
2026-06-11 11:58:27 +0300

    Add reconcile action for creator tip payment recovery.
    - Call Tribetip::Paystack::ReconcileTipPayment with audit context.
    - Return reconciled tip JSON on success or accepted status otherwise.

--- app/controllers/me/tips_controller.rb
+++ app/controllers/me/tips_controller.rb
@@ -4,6 +4,7 @@ module Me
   class TipsController < ApplicationController
     include TipSerializable
     include RequirePaystackOnboarding
+    include AuditRequestContext
 
     before_action :authenticate_tribe!
 
@@ -18,6 +19,31 @@ module Me
       authorize tip
       apply_http_cache_policy(:no_store)
       render json: { tip: tip_json(tip) }
+    end
+
+    def reconcile
+      tip = Tip.find(params[:id])
+      authorize tip, :reconcile?
+
+      apply_http_cache_policy(:no_store)
+      result = Tribetip::Paystack::ReconcileTipPayment.call(
+        tip,
+        paid_via: :reconcile,
+        actor_id: current_tribe.id,
+        request_context: audit_request_context
+      )
+
+      if result.success?
+        render json: {
+          message: "Tip payment reconciled with Paystack.",
+          tip: tip_json(tip.reload)
+        }
+      else
+        render json: {
+          message: result.message,
+          tip: tip_json(tip.reload)
+        }, status: :accepted
+      end
     end
   end
 end
