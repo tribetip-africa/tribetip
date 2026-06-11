8f871f2f1b8bdda235d33b066634ac1afbd257b2
mboya <mboyaberry@gmail.com>
2026-06-11 11:58:27 +0300

    Skip Paystack subaccount provisioning for non-creator tribes.
    - Return early unless tribe.paystack_sync_required?.

--- app/jobs/paystack/provision_subaccount_job.rb
+++ app/jobs/paystack/provision_subaccount_job.rb
@@ -10,7 +10,7 @@ module Paystack
 
     def perform(tribe_id, settlement_bank:, account_number:, business_name: nil)
       tribe = Tribe.find_by(id: tribe_id)
-      return unless tribe
+      return unless tribe&.paystack_sync_required?
 
       run_job_step(tribe_id: tribe_id) do
         result = Tribetip::Paystack::ProvisionSubaccount.call(
