01f386ada223403048af0aab27783f0d8848032b
mboya <mboyaberry@gmail.com>
2026-06-16 13:27:42 +0300

    Refactor public profile controller to use shared renderable concern.

--- app/controllers/public_profiles_controller.rb
+++ app/controllers/public_profiles_controller.rb
@@ -1,44 +1,14 @@
 # frozen_string_literal: true
 
 class PublicProfilesController < ApplicationController
+  include PublicProfileRenderable
+
   def show
-    apply_http_cache_policy(
-      request.headers["Authorization"].present? ? :private_no_store : :public_short
-    )
-
     username = params[:username].to_s.downcase
-    payload = Tribetip::SecureCache.fetch(
-      Tribetip::SecureCache.public_profile_key(username),
-      scope: :public
-    ) do
-      tribe = Tribe.find_by(username: username)
-      raise ActiveRecord::RecordNotFound if tribe.nil?
-      raise ActiveRecord::RecordNotFound unless cacheable_public_profile?(tribe)
+    tribe = Tribe.find_by(username: username)
+    raise ActiveRecord::RecordNotFound unless tribe
+    raise ActiveRecord::RecordNotFound unless cacheable_public_profile?(tribe)
 
-      public_profile_json(tribe)
-    end
-
-    render json: { profile: payload }
-  end
-
-  private
-
-  def cacheable_public_profile?(tribe)
-    tribe.creator? &&
-      tribe.is_profile_public? &&
-      tribe.account_status == "active" &&
-      !tribe.suspended?
-  end
-
-  def public_profile_json(tribe)
-    {
-      id: tribe.id,
-      username: tribe.username,
-      display_name: tribe.display_name,
-      bio: tribe.bio,
-      country_code: tribe.country_code,
-      currency: tribe.currency,
-      default_tip_amount_cents: tribe.default_tip_amount_cents
-    }
+    render_public_profile(tribe)
   end
 end
