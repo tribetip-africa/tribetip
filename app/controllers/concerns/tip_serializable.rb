892338921f33e2251c2e6b5c3190d7cc91df1111
mboya <mboyaberry@gmail.com>
2026-06-16 16:38:17 +0300

    Add public_tip_json serializer that omits supporter contact details.

--- app/controllers/concerns/tip_serializable.rb
+++ app/controllers/concerns/tip_serializable.rb
@@ -29,4 +29,26 @@ module TipSerializable
 
     payload
   end
+
+  def public_tip_json(tip, include_checkout: false)
+    payload = {
+      id: tip.id,
+      tribe_id: tip.tribe_id,
+      amount_cents: tip.amount_cents,
+      currency: tip.currency,
+      status: tip.status,
+      paystack_reference: tip.paystack_reference,
+      paid_at: tip.paid_at,
+      paid_via: tip.paid_via,
+      failed_reason: tip.failed_reason,
+      created_at: tip.created_at
+    }
+
+    if include_checkout
+      payload[:authorization_url] = tip.paystack_metadata["authorization_url"]
+      payload[:checkout_status] = tip.paystack_metadata["checkout_status"]
+    end
+
+    payload
+  end
 end
