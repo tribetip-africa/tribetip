efde63cc44ac9067f3fbf66d0c050a2e877b68b4
mboya <mboyaberry@gmail.com>
2026-06-16 16:38:04 +0300

    Add admin endpoint to list payment alerts with pagination.

---
+++ app/controllers/admin/payment_alerts_controller.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Admin
+  class PaymentAlertsController < BaseController
+    def index
+      apply_http_cache_policy(:no_store)
+
+      alerts = PaymentAlert.recent_first
+      alerts = alerts.unresolved if ActiveModel::Type::Boolean.new.cast(params[:unresolved])
+
+      render json: {
+        alerts: alerts.limit(page_limit).offset(page_offset).map(&:as_json),
+        pagination: {
+          limit: page_limit,
+          offset: page_offset,
+          total: alerts.count
+        }
+      }
+    end
+
+    private
+
+    def page_limit
+      [ [ params.fetch(:limit, 25).to_i, 1 ].max, 100 ].min
+    end
+
+    def page_offset
+      [ params.fetch(:offset, 0).to_i, 0 ].max
+    end
+  end
+end
