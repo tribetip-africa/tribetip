07e1491e40f48ad6da4b0a604dd3dfe94cf28074
mboya <mboyaberry@gmail.com>
2026-06-16 13:27:39 +0300

    Test authenticated share-link fetch and rotate API endpoints.

---
+++ spec/requests/me/share_links_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Creator share links", type: :request do
+  def json
+    JSON.parse(response.body)
+  end
+
+  def bearer_token_for(tribe)
+    token, = Warden::JWTAuth::UserEncoder.new.call(tribe, :tribe, nil)
+    { "Authorization" => "Bearer #{token}" }
+  end
+
+  def create_creator(username: "share_me")
+    tribe = Tribe.new(
+      email: "#{username}@tribetip.africa",
+      password: "securepass123",
+      password_confirmation: "securepass123",
+      username: username,
+      display_name: "Share Me",
+      is_profile_public: true,
+      account_status: "active"
+    )
+    tribe.skip_confirmation!
+    tribe.save!
+    complete_stub_paystack_onboarding!(tribe)
+    tribe.reload
+  end
+
+  it "returns an opaque share link for the signed-in creator" do
+    tribe = create_creator
+
+    get "/me/share_link", headers: bearer_token_for(tribe), as: :json
+
+    expect(response).to have_http_status(:ok)
+    link = json.fetch("share_link")
+    expect(link.fetch("token")).to be_present
+    expect(link.fetch("path")).to start_with("/t/")
+    expect(link.fetch("url")).to include("/t/")
+    expect(link.fetch("url")).not_to include(tribe.username)
+    expect(link.fetch("shareable")).to be(true)
+  end
+
+  it "rotates the share token" do
+    tribe = create_creator(username: "share_rotate_me")
+    get "/me/share_link", headers: bearer_token_for(tribe), as: :json
+    original = json.dig("share_link", "token")
+
+    post "/me/share_link/rotate", headers: bearer_token_for(tribe), as: :json
+
+    expect(response).to have_http_status(:ok)
+    rotated = json.dig("share_link", "token")
+    expect(rotated).not_to eq(original)
+    expect(tribe.reload.tip_share_token).to eq(rotated)
+  end
+end
