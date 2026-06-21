ad02a3695a88aacb165c18750dadcb218c1e625b
mboya <mboyaberry@gmail.com>
2026-06-21 14:54:41 +0300

    Cover public profile, share, and widget path matchers in rack attack specs.

--- spec/lib/tribetip/rack_attack_paths_spec.rb
+++ spec/lib/tribetip/rack_attack_paths_spec.rb
@@ -27,4 +27,36 @@ RSpec.describe Tribetip::RackAttackPaths do
       expect(described_class.auth_path?(request(method: :post, path: "/tips"))).to be(false)
     end
   end
+
+  describe ".public_profile_path?" do
+    def request(method:, path:)
+      instance_double(ActionDispatch::Request, get?: method == :get, path: path)
+    end
+
+    it "matches public creator profile routes" do
+      expect(described_class.public_profile_path?(request(method: :get, path: "/tribes/ama_creates"))).to be(true)
+      expect(described_class.public_profile_path?(request(method: :post, path: "/tribes/ama_creates"))).to be(false)
+    end
+  end
+
+  describe ".share_profile_path?" do
+    def request(method:, path:)
+      instance_double(ActionDispatch::Request, get?: method == :get, path: path)
+    end
+
+    it "matches opaque share profile routes" do
+      token = "abc123-_ABCdef456789012345678"
+      expect(described_class.share_profile_path?(request(method: :get, path: "/share/#{token}"))).to be(true)
+    end
+  end
+
+  describe ".widget_config_path?" do
+    def request(method:, path:)
+      instance_double(ActionDispatch::Request, get?: method == :get, path: path)
+    end
+
+    it "matches the widget config route" do
+      expect(described_class.widget_config_path?(request(method: :get, path: "/widget/config"))).to be(true)
+    end
+  end
 end
