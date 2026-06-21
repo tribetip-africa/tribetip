19e9084f464d9e499a1f5ba90563cdabe7b04a23
mboya <mboyaberry@gmail.com>
2026-06-21 11:47:43 +0300

    Allow profile access when payout is linked but onboarding timestamp is cleared.

--- spec/requests/paystack_onboarding_guard_spec.rb
+++ spec/requests/paystack_onboarding_guard_spec.rb
@@ -39,4 +39,19 @@ RSpec.describe "Paystack onboarding guards", type: :request do
     expect(response).to have_http_status(:forbidden)
     expect(json.dig("error", "code")).to eq("onboarding_required")
   end
+
+  it "allows dashboard profile access when payout account is linked but awaiting Paystack verification" do
+    tribe = create_tribe(username: "guard_linked")
+    tribe.update_columns(
+      paystack_customer_code: "cus_test",
+      paystack_subaccount_code: "acct_test",
+      onboarding_completed_at: nil
+    )
+
+    get "/me/profile", headers: bearer_token_for(tribe), as: :json
+
+    expect(response).to have_http_status(:ok)
+    expect(json.dig("profile", "username")).to eq("guard_linked")
+    expect(tribe.reload.onboarding_completed_at).to be_present
+  end
 end
