53fa54e895e2da2680f886aa15274302f66bb3ef
mboya <mboyaberry@gmail.com>
2026-06-21 14:54:41 +0300

    Simplify widget config lookup by delegating validation to WidgetEmbed.

--- app/controllers/widget_configs_controller.rb
+++ app/controllers/widget_configs_controller.rb
@@ -4,11 +4,7 @@ class WidgetConfigsController < ApplicationController
   def show
     apply_http_cache_policy(:public_short)
 
-    token = params[:token].to_s
-    raise ActiveRecord::RecordNotFound unless Tribetip::WidgetEmbed.valid_token_format?(token)
-    raise ActiveRecord::RecordNotFound if Tribetip::WidgetEmbed.revoked?(token)
-
-    config = Tribetip::WidgetEmbed.resolve_config(token)
+    config = Tribetip::WidgetEmbed.resolve_config(params[:token].to_s)
     raise ActiveRecord::RecordNotFound unless config
 
     render json: { config: config }
