9e301b8cc5ece54422f572887c6d41dbc234b20d
mboya <mboyaberry@gmail.com>
2026-06-21 11:47:43 +0300

    Test tip preset label generation for widget config.

---
+++ spec/lib/tribetip/tip_presets_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Tribetip::TipPresets do
+  it "builds standard, generous, and custom labels" do
+    labels = described_class.labels_for(50_000, "NGN")
+
+    expect(labels).to eq(["₦500", "₦1,000", "Custom"])
+  end
+end
