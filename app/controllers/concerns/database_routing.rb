a2c6c5aa0d3ab540bb57e9b77e0da6f5f72c351a
mboya <mboyaberry@gmail.com>
2026-06-22 11:40:44 +0300

    Route public tip checkout polling to the primary database.
    Checkout reconcile writes tip_events; forcing primary avoids
    ActiveRecord::ReadOnlyError when GET /tips/checkout hits a replica.

--- app/controllers/concerns/database_routing.rb
+++ app/controllers/concerns/database_routing.rb
@@ -19,10 +19,15 @@ module DatabaseRouting
 
   def force_primary_connection?
     return true if request.path == "/up"
+    return true if public_tip_checkout_request?
     return true if devise_auth_request?
     return true if authenticated_api_request?
 
     request.post? || request.put? || request.patch? || request.delete?
+  end
+
+  def public_tip_checkout_request?
+    request.get? && request.path.match?(%r{\A/tips/checkout/})
   end
 
   def authenticated_api_request?
