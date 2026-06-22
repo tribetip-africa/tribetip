6250e35a41436192cb1f11a2cf77c89a38e1eda3
mboya <mboyaberry@gmail.com>
2026-06-22 05:58:54 +0300

    Authorize admin tip investigation endpoint.

--- app/controllers/admin/tips_controller.rb
+++ app/controllers/admin/tips_controller.rb
@@ -3,6 +3,8 @@
 module Admin
   class TipsController < BaseController
     def investigate
+      authorize Tip, :investigate?
+
       apply_http_cache_policy(:no_store)
 
       investigation = Tribetip::Audit::InvestigateTip.call(params[:paystack_reference])
