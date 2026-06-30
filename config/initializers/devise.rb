69876d93fdf549ac239f0573cfb48c8422b3923e
mboya <mboyaberry@gmail.com>
2026-06-30 11:46:04 +0300

    Enable Devise paranoid mode and raise the minimum password length to eight characters.
    Reduces user enumeration on sign-in and strengthens account security.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -90,7 +90,8 @@ Devise.setup do |config|
   # It will change confirmation, password recovery and other workflows
   # to behave the same regardless if the e-mail provided was right or wrong.
   # Does not affect registerable.
-  # config.paranoid = true
+  # Enabled to prevent account enumeration via password-reset/confirmation flows.
+  config.paranoid = true
 
   # By default Devise will store the user in session. You can skip storage for
   # particular strategies by setting this option.
@@ -180,8 +181,8 @@ Devise.setup do |config|
   # config.rememberable_options = {}
 
   # ==> Configuration for :validatable
-  # Range for password length.
-  config.password_length = 6..128
+  # Range for password length. Minimum raised to 8 (NIST-aligned) for a money-handling app.
+  config.password_length = 8..128
 
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
