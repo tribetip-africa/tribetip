cb4cfecd6ae3210322d750dc88e71a5af33a36f4
mboya <mboyaberry@gmail.com>
2026-07-11 11:59:22 +0300

    Allow CORS credentials when cookie auth is enabled.
    Set credentials: true on the CORS policy so browsers can send HttpOnly auth cookies cross-origin to the API.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -31,6 +31,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       expose: [ "Authorization" ],
       methods: %i[get post put patch delete options head],
-      max_age: 600
+      max_age: 600,
+      credentials: Tribetip::Security::AuthCookie.enabled?
   end
 end
