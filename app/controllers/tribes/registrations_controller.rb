16ea097303aa2fbf339182049792dec19d16f441
mboya <mboyaberry@gmail.com>
2026-07-15 15:39:40 +0300

    Require and burn early access invites on gated creator signup.

--- app/controllers/tribes/registrations_controller.rb
+++ app/controllers/tribes/registrations_controller.rb
@@ -11,37 +11,46 @@ module Tribes
       apply_http_cache_policy(:no_store)
       signup_attributes = sign_up_params
       referral_code = signup_attributes.delete(:referral_code)
+      early_access_token = signup_attributes.delete(:early_access_token)
+
+      invite = Tribetip::EarlyAccess::AttachOnSignup.call(
+        email: signup_attributes[:email],
+        token: early_access_token
+      )
+
       build_resource(signup_attributes)
       resource.role = "creator"
       resource.skip_confirmation! unless Tribetip::Security.require_email_confirmation?
-      resource.save
 
-      if resource.persisted?
-        Tribetip::Referrals::AttachOnSignup.call(
-          referred: resource,
-          referral_code: referral_code,
-          signup_ip: request.remote_ip
-        )
-
-        message = if resource.confirmed?
-          "Signed up successfully."
-        else
-          "Signed up successfully. Please confirm your email before signing in."
-        end
-
-        render json: {
-          message: message,
-          tribe: tribe_json(resource.reload),
-          confirmation_required: !resource.confirmed?
-        }, status: :created
-      else
-        render_error(
-          Tribetip::Errors::Validation.new(
-            "Validation failed.",
-            details: { errors: resource.errors.full_messages }
-          )
-        )
+      ActiveRecord::Base.transaction do
+        resource.save!
+        invite&.burn!(tribe: resource)
       end
+
+      Tribetip::Referrals::AttachOnSignup.call(
+        referred: resource,
+        referral_code: referral_code,
+        signup_ip: request.remote_ip
+      )
+
+      message = if resource.confirmed?
+        "Signed up successfully."
+      else
+        "Signed up successfully. Please confirm your email before signing in."
+      end
+
+      render json: {
+        message: message,
+        tribe: tribe_json(resource.reload),
+        confirmation_required: !resource.confirmed?
+      }, status: :created
+    rescue ActiveRecord::RecordInvalid
+      render_error(
+        Tribetip::Errors::Validation.new(
+          "Validation failed.",
+          details: { errors: resource.errors.full_messages }
+        )
+      )
     end
 
     private
@@ -71,7 +80,8 @@ module Tribes
         :display_name,
         :country_code,
         :currency,
-        :referral_code
+        :referral_code,
+        :early_access_token
       )
     end
   end
